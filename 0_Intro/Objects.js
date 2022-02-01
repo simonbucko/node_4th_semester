cebf7752b9c932562b0fe5f670fa496fcc86bb47
SimonBucko <bucino.36@gmail.com>
2022-02-01 15:28:50 +0100

    finished objects

--- 0_Intro/Objects.js
+++ 0_Intro/Objects.js
@@ -8,12 +8,17 @@ const alienMessage = {
 };
 
 // Log the message 
+console.log(alienMessage.message)
 
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
-
+const me = {
+    name: "Simon",
+    age: 21
+}
+console.log(me)
 
 // --------------------------------------
 // Exercise 3 - Add a property 
@@ -21,6 +26,8 @@ const alienMessage = {
 const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
+stackOverflow.isAllowed = true
+console.log(stackOverflow)
 
 // --------------------------------------
 // Exercise 4 - Remove a property 
@@ -30,6 +37,8 @@ const thisSong = {
 }
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute."
-
+delete thisSong.description;
+thisSong.about = "Just a tribute."
+console.log(thisSong)
 
 // --------------------------------------
