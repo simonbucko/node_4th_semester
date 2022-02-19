173a33d1232338c6157e8a9c1a91ea2f5f399c40
SimonBucko <bucino.36@gmail.com>
2022-02-19 14:46:04 +0100

    finished the server

--- 2_Time_Website/app.js
+++ 2_Time_Website/app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express()
 const PORT = process.env.PORT || 8080;
+const { quotes } = require("./quotes.js");
 
 // view engine
 app.set('view engine', 'ejs');
 //set static route for serving files
 app.use(express.static('public'));
 
-app.get("/", (req, res) => res.render('home'))
+/**
+ * Returns a random number between min (inclusive) and max (inclusive)
+ */
+const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+app.get("/", (req, res) => {
+    const randomIndex = getRandomNumber(0, 14)
+    res.render('home', { quote: quotes[randomIndex] })
+})
 
 //had something else running on 8080 port
 app.listen(PORT, () => {
---
+++ 2_Time_Website/quotes.js
@@ -0,0 +1,19 @@
+const quotes = [
+    "The two most powerful warriors are patience and time. – Leo Tolstoy",
+    "Time is money. – Benjamin Franklin.",
+    "Time waits for no one. – Folklore.",
+    "Better three hours too soon than a minute too late. – William Shakespeare.",
+    "Lost time is never found again. – Benjamin Franklin.",
+    "Time is the most valuable thing a man can spend. – Theophrastus.",
+    "Time is the wisest counselor of all. – Pericles.",
+    "The key is in not spending time, but in investing it. – Stephen R. Covey.",
+    "It is the time you have wasted for your rose that makes your rose so important. – Antoine de Saint-Exupéry, The Little Prince.",
+    "Punctuality is the thief of time. – Oscar Wilde, The Picture of Dorian Gray.",
+    "The future is uncertain but the end is always near. Jim Morrison",
+    "Time takes it all, whether you want it to or not. Stephen King, The Green Mile",
+    "Time is a storm in which we are all lost. William Carlos Williams",
+    "The trouble is, you think you have time. Jack Kornfield",
+    "Time brings all things to pass. Aeschylus"
+]
+
+module.exports = { quotes }
