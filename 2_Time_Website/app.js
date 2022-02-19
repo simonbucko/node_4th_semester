79ac18d683f70a36a1737f48bb2cb55589ba1169
SimonBucko <bucino.36@gmail.com>
2022-02-19 12:03:24 +0100

    implemented ejs server

--- 2_Time_Website/app.js
+++ 2_Time_Website/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express()
+const PORT = process.env.PORT || 8080;
 
+// view engine
+app.set('view engine', 'ejs');
+//set static route for serving files
+app.use(express.static('public'));
 
-
-
+app.get("/", (req, res) => res.render('home'))
 
 //had something else running on 8080 port
-app.listen(8080)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
