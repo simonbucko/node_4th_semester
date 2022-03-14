b09ea23c5387f17fc9af27963d887115223e0bc6
SimonBucko <bucino.36@gmail.com>
2022-03-14 22:30:58 +0100

    started work on ppl table

---
+++ 5_People_Random/src/components/PeopleTable.svelte
@@ -0,0 +1,14 @@
+<script>
+  import TableHead from "../../../5_Random_Table/src/components/TableHead.svelte";
+  import { faker } from "@faker-js/faker";
+  export let rows = 10;
+</script>
+
+<table>
+  <TableHead />
+  {#each [...Array(rows).keys()] as index (index)}
+    <li>
+      {index}
+    </li>
+  {/each}
+</table>
