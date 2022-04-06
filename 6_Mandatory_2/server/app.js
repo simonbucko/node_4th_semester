72a1635cddd9c52a0b6d39bae3cabe4908296da2
SimonBucko <bucino.36@gmail.com>
2022-04-06 23:30:18 +0200

    established connection to mongodb

--- 6_Mandatory_2/server/app.js
+++ 6_Mandatory_2/server/app.js
@@ -1,20 +1,30 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv"
-//routes handlers
+import mongoose from "mongoose"
+//routes
 import userRouter from "./routes/usersRouter.js"
+// import authRouter from "./routes/authRouter.js"
 
 dotenv.config()
-const app = express();
-app.use(cors());
-app.use(express.json());
 
-//routers
-app.use("/api/users", userRouter);
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to DB")
+        const app = express()
+        app.use(express.json())
+        app.use(cors())
+        //routers
+        app.use("/api/users", userRouter);
+        // app.use("/api/auth", authRouter)
+        const PORT = process.env.PORT || 8080
+        app.listen(PORT, () => {
+            console.log("Server is runnig on port: ", PORT)
+        })
+    })
+    .catch((error) => {
+        console.log({ error })
+        throw new Error(error)
+    })
 
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log("Starting server on port:", PORT);
-});
---
+++ 6_Mandatory_2/server/routes/authRouter.js
@@ -0,0 +1,125 @@
+import { Router } from "express";
+const router = Router();
+import { body, validationResult } from "express-validator"
+import bcrypt from "bcryptjs"
+import JWT from "jsonwebtoken"
+import { checkAuth } from "../middleware/checkAuth";
+
+
+
+router.post("/signup", body("email").isEmail().withMessage("The email is invalid"), body("password").isLength({ min: 5 }).withMessage("The password should have at least 5 characters"), async (req, res) => {
+    //returns array of errors
+    const validationsErrors = validationResult(req)
+    if (!validationsErrors.isEmpty()) {
+        const errors = validationsErrors.array().map(error => ({
+            msg: error.msg
+        }));
+        return res.json({ errors, data: null })
+    }
+    const { email, password } = req.body;
+    const user = await User.findOne({ email })
+
+    if (user) {
+        return res.json({
+            errors: [
+                {
+                    msg: "Email already in use"
+                }
+            ],
+            data: null
+        })
+    }
+
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = await User.create({
+        email, password: hashedPassword
+    })
+
+    const token = await JWT.sign(
+        { email: newUser.email },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: 343434343434
+        }
+    )
+    res.json({
+        errors: [],
+        data: {
+            token,
+            user: {
+                id: newUser._id,
+                email: newUser.email,
+            }
+        }
+    })
+
+})
+
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email })
+
+    //if user does not exist in db
+    if (!user) {
+        return res.json({
+            errors: [
+                {
+                    msg: "Invalid credentials"
+                }
+            ],
+            data: null
+        })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    //passwords does not match
+    if (!isMatch) {
+        return res.json({
+            errors: [
+                {
+                    msg: "Invalid credentials"
+                }
+            ],
+            data: null
+        })
+    }
+
+    const token = await JWT.sign(
+        { email: user.email },
+        process.env.JWT_SECRET as string,
+        {
+            expiresIn: 343434343434
+        }
+    )
+    res.json({
+        errors: [],
+        data: {
+            token,
+            user: {
+                id: user._id,
+                email: user.email
+            }
+        }
+    })
+
+})
+
+router.get("/me", checkAuth, async (req, res) => {
+    const user = await User.findOne({ email: req.user })
+    return res.json({
+        errors: [],
+        data: {
+            user: {
+                id: user._id,
+                email: user.email,
+                customerStripeId: user.customerStripeId
+            }
+        }
+    })
+
+})
+
+export default router
