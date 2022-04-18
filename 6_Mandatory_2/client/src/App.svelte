7fa7fc8b2dd5277e741926b3f3202542a4aca97c
SimonBucko <bucino.36@gmail.com>
2022-04-18 09:05:05 +0200

    added navbar

--- 6_Mandatory_2/client/src/App.svelte
+++ 6_Mandatory_2/client/src/App.svelte
@@ -1,5 +1,7 @@
 <script>
   import Routing from "./routing/Routing.svelte";
+  import Navbar from "./common/Navbar.svelte";
 </script>
 
+<Navbar />
 <Routing />
---
+++ 6_Mandatory_2/client/src/common/Navbar.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<div class="navbar" />
+
+<style>
+  .navbar {
+    display: flex;
+    padding: 0 48px;
+    background-color: black;
+    height: 64px;
+  }
+</style>
