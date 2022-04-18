d11e892b54b82625fcec4553f3f49ec108debdda
SimonBucko <bucino.36@gmail.com>
2022-04-18 08:59:07 +0200

    created signup page

--- 6_Mandatory_2/client/src/pages/signup/Signup.svelte
+++ 6_Mandatory_2/client/src/pages/signup/Signup.svelte
@@ -1,1 +1,118 @@
-<h1>Signup</h1>
+<script>
+  import Button, { Label } from "@smui/button";
+  import axios from "axios";
+  import { SERVER_API_URL } from "../../common/constants";
+  import Loader from "../../common/Loader.svelte";
+  import Textfield from "@smui/textfield";
+  import { Link } from "svelte-navigator";
+  import { LOGIN, HOME } from "../../routing/constants";
+  import { user } from "../../store/store";
+  import { useNavigate } from "svelte-navigator";
+
+  const navigate = useNavigate();
+  let name = "";
+  let email = "";
+  let password = "";
+  let isProcessingOrder = false;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    isProcessingOrder = true;
+    try {
+      const {
+        data: { data },
+      } = await axios.post(`${SERVER_API_URL}/auth/signup`, {
+        name,
+        email,
+        password,
+      });
+      user.set({
+        ...data.user,
+        isLoading: false,
+        isAuthenticated: true,
+      });
+      sessionStorage.setItem("token", data.user.token);
+    } catch (error) {
+      console.log(error);
+      return;
+    } finally {
+      isProcessingOrder = false;
+      name = "";
+      email = "";
+      password = "";
+      if ($user.isAuthenticated) {
+        navigate(HOME, {
+          replace: true,
+        });
+      }
+    }
+  };
+</script>
+
+<main>
+  <div class="wrapper">
+    <h2>Sign Up</h2>
+    <p>Already have account? Log in <Link to={LOGIN}>here.</Link></p>
+    <form on:submit={handleSubmit}>
+      <Textfield
+        style="width: 100%;"
+        helperLine$style="width: 100%;"
+        bind:value={name}
+        label="User Name"
+        required
+      />
+      <Textfield
+        style="width: 100%;"
+        helperLine$style="width: 100%;"
+        bind:value={email}
+        label="Email"
+        required
+      />
+      <Textfield
+        style="width: 100%;"
+        helperLine$style="width: 100%;"
+        bind:value={password}
+        label="Password"
+        required
+        type="password"
+      />
+      <Button
+        variant="raised"
+        type="submit"
+        style="width: 100%; margin-top: 16px"
+        disabled={false}
+      >
+        <Label>Sign up</Label>
+      </Button>
+    </form>
+  </div>
+</main>
+
+{#if isProcessingOrder}
+  <div class="modal">
+    <Loader />
+  </div>
+{/if}
+
+<style>
+  main,
+  .wrapper {
+    height: 100%;
+    max-width: 1000px;
+    margin: auto;
+  }
+
+  form {
+    margin-top: 48px;
+  }
+
+  .modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: black;
+    opacity: 0.6;
+  }
+</style>
