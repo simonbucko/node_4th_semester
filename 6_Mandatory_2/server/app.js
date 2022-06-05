67b60b0825ec59210761f3ae322ae4b32fb45f06
SimonBucko <bucino.36@gmail.com>
2022-06-05 16:56:48 +0200

    added env

--- 6_Mandatory_2/server/app.js
+++ 6_Mandatory_2/server/app.js
@@ -19,7 +19,8 @@ mongoose.connect(process.env.MONGO_URI)
         app.use(express.json())
         app.use(cors())
         //set up sockets
-        const io = new Server(8001, {
+        const SOCKETS_PORT = process.env.SOCKETS_PORT || 8001
+        const io = new Server(SOCKETS_PORT, {
             cors: {
                 origin: ['http://localhost:8080']
             }
