bf97d7a8a0b85e74ba85a12c0407902ace258d89
SimonBucko <bucino.36@gmail.com>
2022-06-06 11:16:44 +0200

    added chat icon

--- 6_Mandatory_2/client/src/pages/chatRooms/Chatrooms.svelte
+++ 6_Mandatory_2/client/src/pages/chatRooms/Chatrooms.svelte
@@ -5,6 +5,9 @@
   import io from "socket.io-client";
   import { user } from "../../store/store";
   import Loader from "../../common/Loader.svelte";
+  import Button, { Label } from "@smui/button";
+  import { navigate } from "svelte-navigator";
+  import IconButton from "@smui/icon-button";
 
   let chatRooms = [];
   let isLoadingChatRooms = true;
@@ -34,16 +37,29 @@
       <div class="itemsWrapper">
         {#each chatRooms as chatRoom}
           <div class="card">
-            <h3>Custorer: {chatRoom.userName}</h3>
-            <p>Category: {chatRoom.category}</p>
+            <div class="cardRowWrapper">
+              <h3>Custorer: {chatRoom.userName}</h3>
+              <IconButton
+                class="material-icons"
+                ripple={false}
+                title="Unread messages"
+                variant="outlined">mark_chat_unread</IconButton
+              >
+            </div>
+            <div class="cardRowWrapper">
+              <p>Category: {chatRoom.category}</p>
+              <Button
+                variant="raised"
+                on:click={() => navigate(`${PRODUCT}/${productId}`)}
+              >
+                <Label>Reply</Label>
+              </Button>
+            </div>
           </div>
         {/each}
       </div>
     {:else}
-      <p>
-        There are no active waiting rooms. That means, we only have happy
-        customers!
-      </p>
+      <p>There are no active chat rooms!</p>
     {/if}
   </div>
 </main>
@@ -64,6 +80,11 @@
     box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
     border-radius: 12px;
     overflow: hidden;
-    padding: 0 16px;
+    padding: 16px;
+  }
+  .cardRowWrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 </style>
